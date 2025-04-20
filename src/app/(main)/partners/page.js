71702a80d1bd717d9c1bfdd0a7e2190b28a25c4a
shynciee <hungd4842@gmail.com>
2025-04-20 17:06:21 +0700

    fix giao dien employee partner

--- src/app/(main)/partners/page.js
+++ src/app/(main)/partners/page.js
@@ -144,51 +144,24 @@ const data = [
 
 export default function PartnerPage() {
   const [activeTab, setActiveTab] = useState("create");
-  const tableData = jsonToTableFormat(data.slice(0, 5));
+  const tableData = jsonToTableFormat(data.slice(0, 11));
   return (
     <div className="flex flex-col bg-gray-100">
       <h1 className="text-2xl font-semibold text-gray-800 mb-4">
-        Quản lý Đối Tác
+        Quản lý nhân viên
       </h1>
-      <div className="mb-4 border-b border-gray-200">
-        <nav className="-mb-px flex space-x-8 justify-center" aria-label="Tabs">
-          <button
-            onClick={() => setActiveTab("create")}
-            className={`whitespace-nowrap py-3 px-1 border-b-2 font-medium text-sm transition-colors duration-150 ease-in-out focus:outline-none ${
-              activeTab === "create"
-                ? "border-indigo-500 text-indigo-600"
-                : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
-            }`}
-          >
-            Tạo đối tác
-          </button>
-
-          {/* Tab "Quản lý" */}
-          <button
-            onClick={() => setActiveTab("manage")}
-            className={`whitespace-nowrap py-3 px-1 border-b-2 font-medium text-sm transition-colors duration-150 ease-in-out focus:outline-none ${
-              activeTab === "manage"
-                ? "border-indigo-500 text-indigo-600"
-                : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
-            }`}
-          >
-            Quản lý
-          </button>
-        </nav>
-      </div>
-      <div className="flex-grow mt-1px  flex justify-center md:items-start">
-        {activeTab === "create" && <CreatePartnerForm />}
-        {activeTab === "manage" && (
-          <div className="overflow-hidden w-full max-h-[70vh]">
-            <ReuseTable
-              columns={tableData.columns}
-              rows={tableData.rows}
-              currentPage={1}
-              totalPages={200}
-              gridTemplateColumns="60px 200px 300px 200px 200px 200px 200px"
-            />
-          </div>
-        )}
+      <div className="flex flex-col md:flex-row flex-grow gap-4 md:gap-2 md:items-start">
+        <div className="w-full md:w-1/3 lg:w-1/4 flex-shrink-0">
+          <CreatePartnerForm />
+        </div>
+        <div className="flex-grow overflow-hidden">
+          <ReuseTable
+            columns={tableData.columns}
+            rows={tableData.rows}
+            currentPage={1}
+            gridTemplateColumns="60px 200px 300px 200px 200px"
+          />
+        </div>
       </div>
     </div>
   );
