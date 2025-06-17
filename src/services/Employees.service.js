b3d9337be0b87699a8461a962bc4cd84a97b6de2
shynciee <hungd4842@gmail.com>
2025-06-17 22:00:13 +0700

    a

--- src/services/Employees.service.js
+++ src/services/Employees.service.js
@@ -1,52 +1,77 @@
-const { BadRequestError, NotFoundError, InternalServerError, ConflictError } = require('../core/error');
-const { BAD_REQUEST } = require('../core/respone');
-const { executeQuery } = require('../database/executeQuery');
-const { findEmployeeByInfo, findEmployeeByID } = require('../models/repo/employees.repo');
+const {
+  BadRequestError,
+  NotFoundError,
+  InternalServerError,
+  ConflictError,
+} = require("../core/error");
+const { BAD_REQUEST } = require("../core/respone");
+const { executeQuery } = require("../database/executeQuery");
+const {
+  findEmployeeByInfo,
+  findEmployeeByID,
+} = require("../models/repo/employees.repo");
 
 class EmployeeService {
+  // Create employee
+  createEmployee = async (payload) => {
+    const foundEmployee = await findEmployeeByInfo({
+      email: payload.email,
+      phone: payload.phone,
+    });
 
-    // Create employee
-    createEmployee = async (payload) => {
-        const foundEmployee = await findEmployeeByInfo({ email: payload.email, phone: payload.phone });
-
-        if (foundEmployee[0]) {
-            throw new ConflictError("Nhân viên có số điện thoại và email này đã tồn tại!")
-        }
-
-        const query = `
-                    INSERT INTO employees (name, email, phone, user_id)
-                    VALUES (?, ?, ?, ?)
-                `;
-        return await executeQuery(query, [payload.name, payload.email, payload.phone, payload.user_id]);
+    if (foundEmployee[0]) {
+      throw new ConflictError(
+        "Nhân viên có số điện thoại và email này đã tồn tại!"
+      );
     }
 
-    // Update partner
-    updateEmployees = async (payload) => {
-        const foundEmployee = await findEmployeeByID(payload.employee_id);
-        if (!foundEmployee[0]) {
-            throw new NotFoundError("Nhân viên không tồn tại!");
-        }
+    const query = `
+                    INSERT INTO employees (name, email, phone)
+                    VALUES (?, ?, ?)
+                `;
+    return await executeQuery(query, [
+      payload.name,
+      payload.email,
+      payload.phone,
+    ]);
+  };
 
-        const query = `
+  // Update partner
+  updateEmployees = async (payload) => {
+    const foundEmployee = await findEmployeeByID(payload.employee_id);
+    if (!foundEmployee[0]) {
+      throw new NotFoundError("Nhân viên không tồn tại!");
+    }
+
+    const query = `
                 UPDATE employees
                 SET name = ?, email = ?, phone = ?
                 WHERE employee_id = ? AND is_deleted = FALSE
             `;
-        return await executeQuery(query, [payload.name, payload.email, payload.phone, payload.employee_id]);
+    return await executeQuery(query, [
+      payload.name,
+      payload.email,
+      payload.phone,
+      payload.employee_id,
+    ]);
+  };
+
+  // Get all partners
+  getAllEmployees = async (payload) => {
+    const { page, limit } = payload;
+    const offset = (page - 1) * limit;
+    const parsedLimit = parseInt(limit, 10);
+    const parsedPage = parseInt(page, 10);
+    if (
+      isNaN(parsedLimit) ||
+      isNaN(parsedPage) ||
+      parsedLimit <= 0 ||
+      parsedPage <= 0
+    ) {
+      throw new BadRequestError("Limit và page phải là số nguyên dương!");
     }
 
-
-    // Get all partners
-    getAllEmployees = async (payload) => {
-        const { page, limit } = payload;
-        const offset = (page - 1) * limit;
-        const parsedLimit = parseInt(limit, 10);
-        const parsedPage = parseInt(page, 10);
-        if (isNaN(parsedLimit) || isNaN(parsedPage) || parsedLimit <= 0 || parsedPage <= 0) {
-            throw new BadRequestError("Limit và page phải là số nguyên dương!");
-        }
-
-        const query = `
+    const query = `
             SELECT 
                 e.employee_id, 
                 e.name, 
@@ -59,93 +84,97 @@ class EmployeeService {
             WHERE e.is_deleted = FALSE
             ORDER BY e.name ASC
             LIMIT ${parsedLimit} OFFSET ${offset}`;
-        const countQuery = `SELECT COUNT(*) AS total FROM employees WHERE is_deleted = FALSE`;
-        const countResult = await executeQuery(countQuery);
-        const total = countResult[0].total;
-        const results = await executeQuery(query);
+    const countQuery = `SELECT COUNT(*) AS total FROM employees WHERE is_deleted = FALSE`;
+    const countResult = await executeQuery(countQuery);
+    const total = countResult[0].total;
+    const results = await executeQuery(query);
 
-        return {
-            total: total,
-            totalPage: Math.ceil(total / parsedLimit),
-            page: parsedPage,
-            limit: parsedLimit,
-            data: results
-        };
+    return {
+      total: total,
+      totalPage: Math.ceil(total / parsedLimit),
+      page: parsedPage,
+      limit: parsedLimit,
+      data: results,
+    };
+  };
+
+  // Get partner by id
+  getEmployeerById = async (payload) => {
+    const { employee_id } = payload;
+    const query = `SELECT * FROM employees WHERE employee_id = ? AND is_deleted = FALSE`;
+    const employee = await executeQuery(query, [employee_id]);
+    if (!employee[0]) {
+      throw new NotFoundError("Nhân viên không tồn tại!");
     }
 
-    // Get partner by id
-    getEmployeerById = async (payload) => {
-        const { employee_id } = payload;
-        const query = `SELECT * FROM employees WHERE employee_id = ? AND is_deleted = FALSE`;
-        const employee = await executeQuery(query, [employee_id]);
-        if (!employee[0]) {
-            throw new NotFoundError("Nhân viên không tồn tại!");
-        }
+    return employee[0];
+  };
 
-        return employee[0];
+  //
+  getEmployeeName = async () => {
+    const query = `SELECT employee_id, name FROM employees WHERE is_deleted = FALSE`;
+    const employeeNames = await executeQuery(query);
+
+    return employeeNames;
+  };
+
+  deleteEmployee = async (payload) => {
+    const { employee_id } = payload;
+
+    const employeeIds = Array.isArray(employee_id)
+      ? employee_id
+      : [employee_id];
+
+    if (employeeIds.length === 0) {
+      throw new BadRequestError("Danh sách employee_id không hợp lệ!");
     }
 
-    // 
-    getEmployeeName = async () => {
-        const query = `SELECT employee_id, name FROM employees WHERE is_deleted = FALSE`;
-        const employeeNames = await executeQuery(query);
-
-        return employeeNames;
-    }
-
-    deleteEmployee = async (payload) => {
-        const { employee_id } = payload;
-
-        const employeeIds = Array.isArray(employee_id) ? employee_id : [employee_id];
-
-        if (employeeIds.length === 0) {
-            throw new BadRequestError("Danh sách employee_id không hợp lệ!");
-        }
-
-        const placeholders = employeeIds.map(() => '?').join(', ');
-        const findQuery = `
+    const placeholders = employeeIds.map(() => "?").join(", ");
+    const findQuery = `
             SELECT employee_id FROM employees
             WHERE employee_id IN (${placeholders}) AND is_deleted = FALSE;
         `;
-        const foundEmployees = await executeQuery(findQuery, employeeIds);
+    const foundEmployees = await executeQuery(findQuery, employeeIds);
 
-        const foundIds = foundEmployees.map(employee => employee.employee_id);
+    const foundIds = foundEmployees.map((employee) => employee.employee_id);
 
-        const notFoundIds = employeeIds.filter(id => !foundIds.includes(id));
+    const notFoundIds = employeeIds.filter((id) => !foundIds.includes(id));
 
-        if (notFoundIds.length > 0) {
-            throw new NotFoundError(`Không tìm thấy các employee_id sau: ${notFoundIds.join(', ')}`);
-        }
+    if (notFoundIds.length > 0) {
+      throw new NotFoundError(
+        `Không tìm thấy các employee_id sau: ${notFoundIds.join(", ")}`
+      );
+    }
 
-        const deleteQuery = `
+    const deleteQuery = `
             UPDATE employees
             SET is_deleted = TRUE
             WHERE employee_id IN (${placeholders});
         `;
-        return await executeQuery(deleteQuery, employeeIds);
-    }
+    return await executeQuery(deleteQuery, employeeIds);
+  };
 
-    searchEmployee = async (body, query) => {
-        const { search } = body;
-        const { limit, page } = query;
+  searchEmployee = async (body, query) => {
+    const { search } = body;
+    const { limit, page } = query;
 
-        if (search === '' || !search) {
-            return this.getAllEmployees(query);
-        } else {
-            const parsedLimit = parseInt(limit, 10);
-            const parsedPage = parseInt(page, 10);
-            if (
-                isNaN(parsedLimit) ||
-                isNaN(parsedPage) ||
-                parsedLimit <= 0 ||
-                parsedPage <= 0
-            ) {
-                throw new BadRequestError('Limit and page must be positive integers!');
-            }
+    if (search === "" || !search) {
+      return this.getAllEmployees(query);
+    } else {
+      const parsedLimit = parseInt(limit, 10);
+      const parsedPage = parseInt(page, 10);
+      if (
+        isNaN(parsedLimit) ||
+        isNaN(parsedPage) ||
+        parsedLimit <= 0 ||
+        parsedPage <= 0
+      ) {
+        throw new BadRequestError("Limit and page must be positive integers!");
+      }
 
-            const offset = (parsedPage - 1) * parsedLimit;
+      const offset = (parsedPage - 1) * parsedLimit;
 
-            const searchQuery = `
+      const searchQuery = `
                 SELECT 
                     e.employee_id,
                     e.name,
@@ -159,17 +188,15 @@ class EmployeeService {
                 AND e.is_deleted = FALSE
                 LIMIT ${parsedLimit} OFFSET ${offset}
             `;
-            const employees = await executeQuery(searchQuery, [
-                `%${search}%`,
-                `%${search}%`,
-                `%${search}%`,
-            ]);
+      const employees = await executeQuery(searchQuery, [
+        `%${search}%`,
+        `%${search}%`,
+        `%${search}%`,
+      ]);
 
-            return employees;
-        }
-
-
-    };
+      return employees;
+    }
+  };
 }
 
 module.exports = new EmployeeService();
